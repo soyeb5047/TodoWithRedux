6a6489e43ef09da59fdb21ebf71d6036442dfb49
soyeb5047 <soyebsk3861@gmail.com>
2024-07-21 10:56:37 +0530

    todo website using react-redux

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddTodo from './components/AddTodo.jsx'
+import Todo from './components/Todo.jsx'
+function App() {
+
+  return (
+    <>
+      <h1>redux tutorial</h1>
+      <AddTodo/>
+      <Todo />
+    </>
+  )
+}
+
+export default App
